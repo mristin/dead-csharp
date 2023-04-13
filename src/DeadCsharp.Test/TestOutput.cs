d75a2b1149edcc721bd3be7cec4db8b89035a0c8
ignotus87 <sajtosstangli@gmail.com>
2023-04-13 11:41:33 +0200

    Add count by file output option (#29)
    **Changes**
    A new command line option has been added:
    `--output count-by-file`
    
    This will change the output for files containing any dead cues to show the total number of problems in that file in the form:
    `FAIL [path]: [count]`
    
    **Why**
    My intended use for this is to process the output in PowerShell to get the top 10 files with the most probems in order to find the locations where actions would lead to the greatest benefit.
    
    Co-authored-by: Takacs, Zsolt (ext) <zsolt.takacs.ext@siemens-energy.com>

--- src/DeadCsharp.Test/TestOutput.cs
+++ src/DeadCsharp.Test/TestOutput.cs
@@ -23,6 +23,20 @@ namespace DeadCsharp.Test
         }
 
         [Test]
+        public void TestNoSuspectsCountByFile()
+        {
+            string path = "/some/Program.cs";
+
+            using var writer = new StringWriter();
+            bool hasSuspect = Output.ReportCountByFile(path, new List<Inspection.Suspect>(), writer);
+
+            string nl = System.Environment.NewLine;
+
+            Assert.IsFalse(hasSuspect);
+            Assert.AreEqual($"OK   /some/Program.cs{nl}", writer.ToString());
+        }
+
+        [Test]
         public void TestOneSuspectOneCue()
         {
             string path = "/some/Program.cs";
@@ -45,6 +59,30 @@ namespace DeadCsharp.Test
                 $"FAIL /some/Program.cs:{nl}" +
                 $"  * Comment starting at 3:2:{nl}" +
                 $"    * Cue at 4:5: a line contains `something`{nl}",
+                writer.ToString());
+        }
+
+        [Test]
+        public void TestOneSuspectOneCueCountByFile()
+        {
+            string path = "/some/Program.cs";
+            var suspects = new List<Inspection.Suspect>
+            {
+                new Inspection.Suspect(
+                    2, 1,
+                    new List<Inspection.Cue>
+                    {
+                        new Inspection.Cue(new Inspection.Contains( "something"), 3, 4)
+                    })
+            };
+
+            string nl = System.Environment.NewLine;
+            using var writer = new StringWriter();
+            bool hasSuspect = Output.ReportCountByFile(path, suspects, writer);
+
+            Assert.IsTrue(hasSuspect);
+            Assert.AreEqual(
+                $"FAIL /some/Program.cs: 1{nl}",
                 writer.ToString());
         }
 
@@ -76,6 +114,34 @@ namespace DeadCsharp.Test
                 $"  * Comment starting at 3:2:{nl}" +
                 $"    * Cue at 4:5: a line contains `some feature`{nl}" +
                 $"    * Cue at 6:7: a line contains `another feature`{nl}",
+                writer.ToString());
+        }
+
+        [Test]
+        public void TestOneSuspectMultipleCuesCountByFile()
+        {
+            string path = "/some/Program.cs";
+            var suspects = new List<Inspection.Suspect>
+            {
+                new Inspection.Suspect(
+                    2, 1,
+                    new List<Inspection.Cue>
+                    {
+                        new Inspection.Cue(
+                            new Inspection.Contains("some feature"), 3, 4),
+                        new Inspection.Cue(
+                            new Inspection.Contains("another feature"), 5, 6)
+                    })
+            };
+
+            string nl = System.Environment.NewLine;
+
+            using var writer = new StringWriter();
+            bool hasSuspect = Output.ReportCountByFile(path, suspects, writer);
+
+            Assert.IsTrue(hasSuspect);
+            Assert.AreEqual(
+                $"FAIL /some/Program.cs: 2{nl}",
                 writer.ToString());
         }
 
@@ -113,6 +179,39 @@ namespace DeadCsharp.Test
                 $"    * Cue at 4:5: a line contains `some feature`{nl}" +
                 $"  * Comment starting at 13:2:{nl}" +
                 $"    * Cue at 14:15: a line contains `another feature`{nl}",
+                writer.ToString());
+        }
+
+        [Test]
+        public void TestMultipleSuspectsCountByFile()
+        {
+            string path = "/some/Program.cs";
+            var suspects = new List<Inspection.Suspect>()
+            {
+                new Inspection.Suspect(
+                    2, 1,
+                    new List<Inspection.Cue>
+                    {
+                        new Inspection.Cue(
+                            new Inspection.Contains("some feature"), 3, 4)
+                    }),
+                new Inspection.Suspect(
+                    12, 1,
+                    new List<Inspection.Cue>
+                    {
+                        new Inspection.Cue(
+                            new Inspection.Contains("another feature"), 13, 14)
+                    })
+            };
+
+            string nl = System.Environment.NewLine;
+
+            using var writer = new StringWriter();
+            bool hasSuspect = Output.ReportCountByFile(path, suspects, writer);
+
+            Assert.IsTrue(hasSuspect);
+            Assert.AreEqual(
+                $"FAIL /some/Program.cs: 2{nl}",
                 writer.ToString());
         }
     }
